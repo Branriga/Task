e7f608a6226d3d5bd580c7ec4ed231387d59f205
Branriga <bryancampoverde7@hotmail.com>
2025-09-27 19:59:04 -0500

    tercer commit

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,10 +5,10 @@ from src.calculator import *
 
 class Testcalculator(unittest.TestCase):
   def test_suma(self):
-    self.assertEqual(suma(2,3), 5)
+    self.assertEqual(suma(20,3), 5)
 
   def test_resta(self):
-   self.assertEqual(resta(5,3), 2)
+   self.assertEqual(resta(50,3), 2)
 
 if __name__ == '__main__':
   unittest.main()
